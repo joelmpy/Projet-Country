f3fc262d615c89bfcf8cd56ae068850baaf7c1d2
Joel <joel_mpy@hotmail.com>
2022-07-18 21:11:54 +0200

    cards

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,56 +1,80 @@
 function Card(props) {
   return (
-	<article className="cards">
-    <div className="cards-flag">
-      <h1>{props.data.name.common}</h1>
-      <img src={props.data.flags.png} />
-      <div className="cards-details">
-        <h4>
-          <span
-            className="material-icons"
-            style={{ height: "100xpx", marginRight: "10px" }}
-          >
-            public
-          </span>
-          Region : <span>{props.data.region}</span>
-        </h4>
-        <h4>
-          <span
-            class="material-icons"
-            style={{ height: "100xpx", marginRight: "10px" }}
-          >
-            flag
-          </span>
-          Capital : <span>{props.data.capital}</span>
-        </h4>
-        <h4>
-          <span
-            class="material-icons"
-            style={{ height: "100xpx", marginRight: "10px" }}
-          >
-            person
-          </span>
-          Population :{" "}
-          <span>
-            {String(props.data.population).replace(/(.)(?=(\d{3})+$)/g, "$1,")}{" "}
-            {props.data.population > 1000000 ? "millions" : "millier"}{" "}
-            d'habitants
-          </span>
-        </h4>
+    <article className="cards">
+      <div className="cards-flag">
+        <h1>{props.data.name.common}</h1>
+        <img src={props.data.flags.png} />
+        <div className="cards-details">
+          <h4>
+            <span
+              className="material-icons"
+              style={{ height: "100xpx", marginRight: "10px" }}
+            >
+              public
+            </span>
+            Region : <span>{props.data.region}</span>
+          </h4>
+          <h4>
+            <span
+              class="material-icons"
+              style={{ height: "100xpx", marginRight: "10px" }}
+            >
+              flag
+            </span>
+            Capital : <span>{props.data.capital}</span>
+          </h4>
+          <h4>
+            <span
+              class="material-icons"
+              style={{ height: "100xpx", marginRight: "10px" }}
+            >
+              person
+            </span>
+            Population :{" "}
+            <span>
+              {String(props.data.population).replace(
+                /(.)(?=(\d{3})+$)/g,
+                "$1,"
+              )}{" "}
+              {props.data.population > 1000000 ? "millions" : "millier"}{" "}
+              d'habitants
+            </span>
+          </h4>
 
-        <h4>
-          <span
-            class="material-icons"
-            style={{ height: "100xpx", marginRight: "10px" }}
-          >
-            flag
-          </span>
-          Subregion: <span>{props.data.subregion}</span>
-        </h4>
+          <h4>
+            <span
+              class="material-icons"
+              style={{ height: "100xpx", marginRight: "10px" }}
+            >
+              flag
+            </span>
+            Subregion: <span>{props.data.subregion}</span>
+          </h4>
+          <h4>
+            <span
+              class="material-icons"
+              style={{ height: "100xpx", marginRight: "10px" }}
+            >
+              translate
+            </span>
+            languages :{" "}
+            <span>
+              {Object.keys(props.data.languages).map((key) => {
+                console.log(key);
+                return key + " ";
+              })}
+            </span>
+          </h4>
 
+          <h4>
+            <iframe className="maps"
+              src={props.data.maps.googleMaps}
+             
+            ></iframe>
+          </h4>
+        </div>
       </div>
-    </div>
-	</article>
+    </article>
   );
 }
 
